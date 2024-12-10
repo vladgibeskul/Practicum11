138a55753a0a366108829cbf9d524844d94d9a98
vladgibeskul <wgv96300@gmail.com>
2024-12-10 20:03:46 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты вершин пятиугольника:");
+            Console.Write("x1, y1: ");
+            var (x1, y1) = ReadCoordinates();
+            Console.Write("x2, y2: ");
+            var (x2, y2) = ReadCoordinates();
+            Console.Write("x3, y3: ");
+            var (x3, y3) = ReadCoordinates();
+            Console.Write("x4, y4: ");
+            var (x4, y4) = ReadCoordinates();
+            Console.Write("x5, y5: ");
+            var (x5, y5) = ReadCoordinates();
+
+            double area1 = CalculateTriangleArea(x1, y1, x2, y2, x3, y3);
+            double area2 = CalculateTriangleArea(x1, y1, x3, y3, x4, y4);
+            double area3 = CalculateTriangleArea(x1, y1, x4, y4, x5, y5);
+
+            double pentagonArea = area1 + area2 + area3;
+
+            Console.WriteLine($"Площадь пятиугольника: {pentagonArea}");
+            Console.ReadLine();
+        }
+        static (double x, double y) ReadCoordinates()
+        {
+            string[] input = Console.ReadLine().Split(',');
+            double x = double.Parse(input[0].Trim());
+            double y = double.Parse(input[1].Trim());
+            return (x, y);
+        }
+
+        static double CalculateTriangleArea(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            return Math.Abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0);
+        }
+    }
+}
---
+++ ConsoleApp5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ConsoleApp5")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e998da42-86a6-48e9-9b2a-f1b79ea39448")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
